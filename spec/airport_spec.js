d1bcc8769aca618685d0ad644e87672867c2808b
Freddie <fredjlawson@gmail.com>
2018-09-04 18:30:15 +0100

    adds a plane in the array from the outset when an airport is instantiated - this allows Airport.takeOff to be tested during stormy weather

--- spec/airport_spec.js
+++ spec/airport_spec.js
@@ -4,35 +4,50 @@ describe ('Airport', function() {
     // spyOn(heathrow, 'isStormy').and.returnValue(false)
   });
   it ('show the planes it has', function() {
-    expect(heathrow.planes.length).toEqual(0)
+    expect(heathrow.planes.length).toEqual(1)
   });
-  
-  describe ('#land', function() {
-    it ('can land a plane', function() {
-      heathrow.land('plane1')
-      expect(heathrow.planes.length).toEqual(1)
+
+
+
+  describe ('Sunny weather - everything can proceed as normal', function () {
+    beforeEach(function() {
+      heathrow = new Airport(5);
+      spyOn(heathrow, 'isStormy').and.returnValue(false)
+    });
+    describe ('#land', function() {
+      it ('can land a plane', function() {
+        heathrow.land('plane1')
+        expect(heathrow.planes.length).toEqual(2)
+        });
+      it ('cannot land if the airport is at capacity', function() {
+        for(var i = 0; i < 4; i++) { heathrow.land(`plane${i}`) }
+        expect(function() { heathrow.land('plane5') }).toThrowError("Airport is full!");
       });
-    it ('cannot land if the airport is at capacity', function() {
-      gatwick = new Airport(5)
-      for(var i = 0; i < 5; i++) {
-        gatwick.land(`plane${i}`)
-      }
-      expect(function() {
-        gatwick.land('plane5');
-      }).toThrowError("Airport is full!"); 
+    });
+    describe ('#takeOff', function() {
+      it ('can take off a plane', function() {
+        heathrow.land('plane2')
+        heathrow.takeOff()
+        expect(heathrow.planes.length).toEqual(1)
+      });
+      it ('cannot take off a plane if the airport is empty', function() {
+        heathrow.takeOff('plane1')
+        expect(function() { heathrow.takeOff() }).toThrowError("No planes in the airport!");
+      });
     });
   });
-  
-  describe ('#takeOff', function() {
-    it ('can take off a plane', function() {
-      heathrow.land('plane1')
-      heathrow.takeOff()
-      expect(heathrow.planes.length).toEqual(0)
+
+
+  describe ('stormy weather - no taking off or landing', function() {
+    beforeEach(function() {
+      heathrow = new Airport(5);
+      spyOn(heathrow, 'isStormy').and.returnValue(true)
     });
-    it ('cannot take off a plane if the airport is empty', function() {
-      expect(function() {
-        heathrow.takeOff();
-      }).toThrowError("No planes in the airport!");
+    it ('will not land a plane', function() {
+      expect(function() { heathrow.land('plane5') }).toThrowError("No landing here, too stormy!");
+    });
+    it ('will not take off a plane', function () {
+      expect(function() { heathrow.takeOff('plane1') }).toThrowError("No taking off, too stormy!");
     });
   });
 
@@ -41,19 +56,6 @@ describe ('Airport', function() {
     it ('can return sunny or stormy', function() {
       expect([true, false]).toContain(heathrow.isStormy())
     });
-
-    it ('isStormySpy', function () {
-      spyOn(heathrow, 'isStormy').and.returnValue(false)
-      expect(heathrow.isStormy()).toEqual(false)
-    });
-
-
   });
-
-  
-  
-
-
-
   
 }); 
--- src/airport.js
+++ src/airport.js
@@ -1,25 +1,31 @@
 function Airport(capacity) {
-  this.planes = [];
+  this.planes = ['plane1'];
   this.capacity = capacity
 }
 
 Airport.prototype.land = function(plane) {
-  if(this.planes.length < this.capacity) {
-    this.planes.push(plane)
+  if(this.isStormy()) {
+    throw new Error("No landing here, too stormy!")
   } else {
-    throw new Error("Airport is full!")
+    if(this.planes.length < this.capacity) {
+      this.planes.push(plane)
+    } else {
+      throw new Error("Airport is full!")
+    }
   }
-  
 }
 
 Airport.prototype.takeOff = function() {
-  if(this.planes.length > 0) {
-    this.planes.pop()
+  if(this.isStormy()) {
+    throw new Error("No taking off, too stormy!")
   } else {
-    throw new Error("No planes in the airport!")
+    if(this.planes.length > 0) {
+      this.planes.pop()
+    } else {
+      throw new Error("No planes in the airport!")
+    }
   }
 }
-
 
 
 Airport.prototype.isStormy = function () {

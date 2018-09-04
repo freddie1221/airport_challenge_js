2aa5b340a0cd94bf002777088b6afd6af3be23e7
Freddie <fredjlawson@gmail.com>
2018-09-04 16:31:56 +0100

    adds random isStormy function, passing tests

--- spec/airport_spec.js
+++ spec/airport_spec.js
@@ -1,14 +1,53 @@
-describe ('Airport',function() {
-
-  it ('', function() {
-
+describe ('Airport', function() {
+  beforeEach(function() {
+    heathrow = new Airport(5);
   });
   
-  it ('', function() {
-    
+  it ('show the planes it has', function() {
+    expect(heathrow.planes.length).toEqual(0)
   });
   
-  it ('', function() {
-    
+  describe ('#land', function() {
+
+    it ('can land a plane', function() {
+      heathrow.land('plane1')
+      expect(heathrow.planes.length).toEqual(1)
+      });
+
+    it ('cannot land if the airport is at capacity', function() {
+      gatwick = new Airport(5)
+      for(var i = 0; i < 5; i++) {
+        gatwick.land(`plane${i}`)
+      }
+      expect(function() {
+        gatwick.land('plane5');
+      }).toThrowError("Airport is full!"); 
+    });
   });
+  
+  describe ('#takeOff', function() {
+
+    it ('can take off a plane', function() {
+      heathrow.land('plane1')
+      heathrow.takeOff()
+      expect(heathrow.planes.length).toEqual(0)
+    });
+  
+    it ('cannot take off a plane if the airport is empty', function() {
+      expect(function() {
+        heathrow.takeOff();
+      }).toThrowError("No planes in the airport!");
+    });
+  });
+
+
+  describe ('isStormy', function() {
+
+    it ('can return sunny or stormy', function() {
+
+       expect([true, false]).toContain(isStormy())
+    });
+  });
+
+  
 }); 
---
+++ src/airport.js
@@ -0,0 +1,34 @@
+function Airport(capacity) {
+  this.planes = [];
+  this.capacity = capacity
+}
+
+Airport.prototype.land = function(plane) {
+  if(this.planes.length < this.capacity) {
+    this.planes.push(plane)
+  } else {
+    throw new Error("Airport is full!")
+  }
+  
+}
+
+Airport.prototype.takeOff = function() {
+  if(this.planes.length > 0) {
+    this.planes.pop()
+  } else {
+    throw new Error("No planes in the airport!")
+  }
+}
+
+var isStormy = function () {
+  if(randNumber(4) < 3) {
+    return false
+  } else {
+    return true
+  }
+}
+
+
+var randNumber = function (max) {
+  return Math.floor(Math.random()*Math.floor(max))
+}
